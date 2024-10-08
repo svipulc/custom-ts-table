63870436118a578ff3ae0da97f41f74d72b96ad7
svipulc <vipul.c@simformsolutions.com>
2024-10-08 17:47:12 +0530

    chore: setup husky, eslint and commitizen

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,43 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import { resolve } from "path";
+import { peerDependencies } from "./package.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+      staticImport: true,
+      outDir: "dist/types",
+      exclude: ["**/example/**/*", "**/data/**/*"],
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "custom-table",
+      formats: ["es", "cjs"],
+      fileName: format => `${format}/index.js`,
+    },
+    rollupOptions: {
+      external: [...Object.keys(peerDependencies)],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          typescript: "Typescript",
+        },
+      },
+    },
+    sourcemap: process.env.NODE_ENV !== "production",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+});
